593bea10ae62bb4474aae6ea21e7cb7862a4d05f
honmaple <xiyang0807@gmail.com>
2017-02-15 00:50:57 +0800

    update

--- config.example/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# *************************************************************************
-#   Copyright © 2015 JiangLin. All rights reserved.
-#   File Name: __init__.py
-#   Author:JiangLin
-#   Mail:xiyang0807@gmail.com
-#   Created Time: 2015-11-18 07:20:38
-# *************************************************************************
